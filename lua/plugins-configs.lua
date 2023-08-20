4a9cfd7e6b1c91d52cf1fe801f2c6196d4e30b1c
Serhii Slavin <slavin.dev@gmail.com>
2023-08-20 12:31:32 +0300

    Update plugins-configs.lua

--- lua/plugins-configs.lua
+++ lua/plugins-configs.lua
@@ -4,9 +4,6 @@ require("nvim-tree").setup({
   renderer = {
     group_empty = true,
   },
-  filters = {
-    dotfiles = true,
-  },
 })
 
 local function open_nvim_tree(data)
@@ -34,6 +31,44 @@ vim.g.AutoPairsFlyMode = 0
 vim.g.AutoPairsMultilineClose = 0
 
 
--- Airline
-vim.g.airline_theme='catppuccin'
+-- Tree Sittter
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "typescript", "tsx", "lua", "html", "javascript", "org", "norg" },
+  highlight = {
+    enable = true,
+    auto_install = true,
+    additional_vim_regex_highlighting = {'org', 'norg'},
+  },
+  indent = { enable = true },
+}
 
+-- Telescope
+require('telescope').load_extension('dir')
+require('telescope').load_extension('live_grep_args')
+
+-- Colorized
+require('colorizer').setup()
+
+-- Neogit
+local neogit = require('neogit')
+neogit.setup {
+  auto_refresh = true,
+  integrations = {
+    diffview = true
+  }
+}
+
+-- NEOrg
+require('neorg').setup {
+ load = {
+    ["core.defaults"] = {},
+    ["core.dirman"] = {
+      config = {
+        workspaces = {
+          home = "~/workspace/orgs",
+        }
+      }
+    },
+    ["core.ui.calendar"] = {}
+  }
+}
